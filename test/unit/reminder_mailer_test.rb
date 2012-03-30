b02fae513328139908dd63be77b79336f2febc1b
Oleg Kandaurov <kandaurovoleg@gmail.com>
2012-03-30 16:56:04 +0400

    Tests have been rewritten with should

--- test/unit/reminder_mailer_test.rb
+++ test/unit/reminder_mailer_test.rb
@@ -3,33 +3,45 @@ require File.dirname(__FILE__) + '/../test_helper'
 class ReminderMailerTest < ActiveSupport::TestCase
   fixtures :users
 
-  def test_redmine_fixtures
-    assert_raise ActiveRecord::RecordNotFound do
-      User.find(1)
+  context "custom fixtures" do
+
+    should "not found an user defined in redmine fixture" do
+      assert_raise ActiveRecord::RecordNotFound do
+        User.find(1)
+      end
     end
+
+    should "find an user defined in plugin fixtures" do
+      assert_equal 'Ivan', User.find(10).firstname
+    end
+
   end
 
-  def test_plugin_fixtures
-    User.find(10)
+  context "reminder settings" do
+
+    setup do
+      Reminder::SettingPatch.default_reminder_notification = '1,2,3,4'
+    end
+
+    should "return setting explicitly defined for user" do
+      assert_equal '1,3,5', User.find(10).reminder_notification
+    end
+
+    should "convert settings to integer array" do
+      assert_equal [1,3,5], User.find(10).reminder_notification_array
+    end
+
+    should "return default settings" do
+      assert_equal '1,2,3,4', Setting.reminder_notification
+    end
+
+    should "return default settings for user" do
+      assert_equal '1,2,3,4', User.find(11).reminder_notification
+    end
+
   end
 
-  def test_get_user_settings
-    assert_equal '1,3,5', User.find(10).reminder_notification
-    assert_equal [1,3,5], User.find(10).reminder_notification_array
-  end
-
-  def test_get_default_settings
-    Reminder::SettingPatch.default_reminder_notification = '1,2,3,4'
-    assert_equal '1,2,3,4', Setting.reminder_notification
-  end
-
-  def test_get_default_user_settings
-    Reminder::SettingPatch.default_reminder_notification = '1,2,3,4'
-    assert_equal '1,2,3,4', User.find(11).reminder_notification
-  end
-
-
-  def test_simple
+  should "perform simple test" do
     expected =
         [
             {
@@ -48,6 +60,5 @@ class ReminderMailerTest < ActiveSupport::TestCase
             }
         ]
       assert_equal(expected, ReminderMailer.eval_mail_data)
-
   end
 end
