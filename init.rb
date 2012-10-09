4c8e0baff9faf53285a29b9f53ea3dc080170279
Oleg Kandaurov <kandaurovoleg@gmail.com>
2012-10-09 12:10:40 +0400

    Prevent users from installing development version

--- init.rb
+++ init.rb
@@ -32,5 +32,6 @@ Redmine::Plugin.register :due_date_reminder do
   version '0.1.1'
   url 'https://github.com/f0y/due_date_reminder'
   author_url 'http://okandaurov.info'
+  requires_redmine :version => '0.0.0'
   settings :default => {'reminder_notification' => '1,3,5'}, :partial => 'reminder/settings'
 end
